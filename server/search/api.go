d63c3f961d15eef39e37bf0610c66b6e2fd185e1
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-10-10 02:49:42 +0200

    cleaning search code

--- server/search/api.go
+++ server/search/api.go
@@ -5,17 +5,6 @@ import (
 	"github.com/NordGus/shrtnr/server/storage/url"
 )
 
-type signal struct {
-	term  string
-	longs []string
-	urls  []url.URL
-	err   error
-}
-
-func (s signal) Error() error {
-	return s.err
-}
-
 func SearchURLs(term string) ([]url.URL, error) {
 	select {
 	case <-ctx.Done():
@@ -30,20 +19,4 @@ func SearchURLs(term string) ([]url.URL, error) {
 
 		return resp.urls, resp.err
 	}
-}
-
-func buildSignal(term string) signal {
-	return signal{term: term}
-}
-
-func getLongsFromCache(sig signal) signal {
-	sig.longs, sig.err = cache.FindEntries(sig.term, longsLimit)
-
-	return sig
-}
-
-func getRecordsFromRepository(sig signal) signal {
-	sig.urls, sig.err = repository.GetLikeLongs(sig.longs...)
-
-	return sig
 }
---
+++ server/search/searchurls.go
@@ -0,0 +1,30 @@
+package search
+
+import "github.com/NordGus/shrtnr/server/storage/url"
+
+type signal struct {
+	term  string
+	longs []string
+	urls  []url.URL
+	err   error
+}
+
+func (s signal) Error() error {
+	return s.err
+}
+
+func buildSignal(term string) signal {
+	return signal{term: term}
+}
+
+func getLongsFromCache(sig signal) signal {
+	sig.longs, sig.err = cache.FindEntries(sig.term, longsLimit)
+
+	return sig
+}
+
+func getRecordsFromRepository(sig signal) signal {
+	sig.urls, sig.err = repository.GetLikeLongs(sig.longs...)
+
+	return sig
+}
