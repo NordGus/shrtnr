4008456040c0f726ff8f0bcd9156d2c711b4812d
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-10-10 02:57:00 +0200

    cleaning redirect code

--- server/redirect/api.go
+++ server/redirect/api.go
@@ -1,25 +1,9 @@
 package redirect
 
 import (
-	"errors"
 	"github.com/NordGus/shrtnr/server/shared/response"
 	"net/http"
-	"strings"
 )
-
-var (
-	InvalidPathErr = errors.New("redirect: invalid path format")
-)
-
-type signal struct {
-	short string
-	full  string
-	err   error
-}
-
-func (s signal) Error() error {
-	return s.err
-}
 
 func GetTarget(r *http.Request) (string, error) {
 	select {
@@ -31,26 +15,4 @@ func GetTarget(r *http.Request) (string, error) {
 
 		return resp.full, resp.err
 	}
-}
-
-func extractShortFromPath(r *http.Request) signal {
-	path := strings.Split(r.URL.Path, "/")
-	if len(path) < 2 {
-		return signal{err: InvalidPathErr}
-	}
-
-	return signal{short: path[1]}
-}
-
-func searchFullURL(sig signal) signal {
-	record, err := repository.GetByShort(sig.short)
-	if err != nil {
-		sig.err = err
-
-		return sig
-	}
-
-	sig.full = record.Full()
-
-	return sig
 }
---
+++ server/redirect/gettarget.go
@@ -0,0 +1,42 @@
+package redirect
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+var (
+	InvalidPathErr = errors.New("redirect: invalid path format")
+)
+
+type signal struct {
+	short string
+	full  string
+	err   error
+}
+
+func (s signal) Error() error {
+	return s.err
+}
+
+func extractShortFromPath(r *http.Request) signal {
+	path := strings.Split(r.URL.Path, "/")
+	if len(path) < 2 {
+		return signal{err: InvalidPathErr}
+	}
+
+	return signal{short: path[1]}
+}
+
+func searchFullURL(sig signal) signal {
+	record, err := repository.GetByShort(sig.short)
+	if err != nil {
+		sig.err = errors.Join(sig.err, err)
+		return sig
+	}
+
+	sig.full = record.Full()
+
+	return sig
+}
