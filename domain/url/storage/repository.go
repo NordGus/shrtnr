baefc2d52b6376c89f3138d773e4033642cc89f7
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-10-15 23:34:06 +0200

    updating repository to return a not implemented error

--- domain/url/storage/repository.go
+++ domain/url/storage/repository.go
@@ -1,9 +1,11 @@
 package storage
 
 import (
-	"github.com/jmoiron/sqlx"
+	"errors"
 
 	"github.com/NordGus/shrtnr/domain/url/entities"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type Repository struct {
@@ -18,35 +20,35 @@ func newStorage(db *sqlx.DB) *Repository {
 
 func (repo *Repository) GetByUUID(uuid entities.UUID) (entities.URL, error) {
 	// TODO: Implement
-	return entities.URL{}, nil
+	return entities.URL{}, errors.New("storage: not implemented")
 }
 
 func (repo *Repository) GetByTarget(target entities.Target) (entities.URL, error) {
 	// TODO: Implement
-	return entities.URL{}, nil
+	return entities.URL{}, errors.New("storage: not implemented")
 }
 
 func (repo *Repository) CreateURL(entity entities.URL) (entities.URL, error) {
 	// TODO: Implement
-	return entities.URL{}, nil
+	return entities.URL{}, errors.New("storage: not implemented")
 }
 
 func (repo *Repository) DeleteURL(id entities.ID) (entities.URL, error) {
 	// TODO: Implement
-	return entities.URL{}, nil
+	return entities.URL{}, errors.New("storage: not implemented")
 }
 
 func (repo *Repository) GetURLsThatMatchTargets(matchTargets ...string) ([]entities.URL, error) {
 	// TODO: Implement
-	return []entities.URL{}, nil
+	return []entities.URL{}, errors.New("storage: not implemented")
 }
 
 func (repo *Repository) GetByID(id entities.ID) (entities.URL, error) {
 	// TODO: Implement
-	return entities.URL{}, nil
+	return entities.URL{}, errors.New("storage: not implemented")
 }
 
 func (repo *Repository) GetAllInPage(page uint, perPage uint) ([]entities.URL, error) {
 	// TODO: Implement
-	return []entities.URL{}, nil
+	return []entities.URL{}, errors.New("storage: not implemented")
 }
