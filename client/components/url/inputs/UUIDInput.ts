1e48c95f52981e7e00561e3a116d4522aa52fe67
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-10-22 17:07:56 +0200

    removing console.log statements

--- client/components/url/inputs/UUIDInput.ts
+++ client/components/url/inputs/UUIDInput.ts
@@ -2,12 +2,9 @@ import { generateUUID } from "@/helpers/uuidGeneration.ts";
 
 class UUIDInput extends HTMLInputElement {
   connectedCallback(): void {
-    console.log(this.value)
     if (this.value || this.value !== "") return
 
     this.value = generateUUID(8)
-
-    console.log(this.value)
   }
 }
 
