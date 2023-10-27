92882f5c554bea0a22e3b73be1c1da6af7e82fe0
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-10-28 01:18:48 +0200

    cleaning documentation

--- domain/shared/middleware/cors.go
+++ domain/shared/middleware/cors.go
@@ -2,7 +2,7 @@ package middleware
 
 import "net/http"
 
-// CORS TODO: develop a better implementation
+// CORS is a middleware to handle Cross-origin resource sharing in development environment
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
